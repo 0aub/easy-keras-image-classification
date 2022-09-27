d011983e1260c405c027067cac4b5a99978a04d9
Ayyub Alzahem <46811238+0aub@users.noreply.github.com>
2022-09-27 09:58:39 +0300

    Add files via upload

--- keras-apps.py
+++ keras-apps.py
@@ -228,11 +228,13 @@ def data_generators(train_path, val_path, input_size, batch_size, aug, seed):
     # create training and validation generators
     # generators are basically like pytorch data loaders
     train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
         rotation_range=20,
         width_shift_range=0.2,
         height_shift_range=0.2,
-        horizontal_flip=True,
-        validation_split=0.2
+        zoom_range=0.2,
+        horizontal_flip=True
         ) if aug else ImageDataGenerator()
     train_generator = train_datagen.flow_from_directory(
         train_path,
