6a980424a2652bc96dbd7a9872d4b423404b1232
Ayyub Alzahem <46811238+0aub@users.noreply.github.com>
2022-10-03 13:25:31 +0300

    Add files via upload

--- keras-apps.py
+++ keras-apps.py
@@ -353,21 +353,21 @@ def y_true_pred(model, x, y):
     speed = time.time() - start
     return (y, preds, speed)
 
-def recall(y_true, y_pred):
+def recall_measure(y_true, y_pred):
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
     possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
     recall = true_positives / (possible_positives + K.epsilon())
     return recall
 
-def precision(y_true, y_pred):
+def precision_measure(y_true, y_pred):
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
     predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
     precision = true_positives / (predicted_positives + K.epsilon())
     return precision
 
-def f1(y_true, y_pred):
-    precision = precision(y_true, y_pred)
-    recall = recall(y_true, y_pred)
+def f1_measure(y_true, y_pred):
+    precision = precision_measure(y_true, y_pred)
+    recall = recall_measure(y_true, y_pred)
     return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
 def measure(title, y_true, y_pred):
